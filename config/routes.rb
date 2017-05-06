0e72c12672afbd741e3b08c508bb12b46bd51705
sogawa55 <s.ogawa055@gmail.com>
2017-05-06 13:08:48 +0000

    Finish user edit, update, index, and destroy actions

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
   get    '/login',   to: 'sessions#new'
-  post '/signup',  to: 'users#create'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   resources :users
