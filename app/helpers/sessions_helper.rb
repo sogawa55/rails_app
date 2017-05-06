765b341b6ed16b17cb83b75ba00201ecde2c8d7c
sogawa55 <s.ogawa055@gmail.com>
2017-05-06 12:00:42 +0000

    Use SSL and the Puma webserver in production

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,6 +4,10 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+  
+  def current_user?(user)
+    user == current_user
+  end
 
   # 現在ログイン中のユーザーを返す (いる場合)
   def current_user
@@ -18,6 +22,17 @@ module SessionsHelper
   def log_out
     session.delete(:user_id)
     @current_user = nil
+  end
+  
+   # 記憶したURL (もしくはデフォルト値) にリダイレクト
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # アクセスしようとしたURLを覚えておく
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
   
 end
